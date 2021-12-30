89dec99567b22d643d70d96e0f1469dadf1481ea
MunchDuster <72060614+MunchDuster@users.noreply.github.com>
2021-12-30 13:56:34 +1300

    Upgraded project to URP, changed colors and fog.

---
+++ Assets/Scripts/CustomGravity.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CustomGravity : MonoBehaviour
+{
+	public Vector3 direction = Vector3.down;
+	public float magnitude = 9.81f;
+
+	private Rigidbody rb;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		rb.AddForce(direction * magnitude, ForceMode.Acceleration);
+	}
+}
